1eb5bf35ec5e52a6211a843c9a0c304e202f8f84
CharlesLove <crashbanito@hotmail.com>
2021-09-04 21:06:51 -0500

    Make sure demo_forcedisablebob AND demoplayback are active instead of just forcedisablebob.

--- prboom2/src/p_user.c
+++ prboom2/src/p_user.c
@@ -223,7 +223,7 @@ void P_CalcHeight (player_t* player)
 
   angle = (FINEANGLES / 20 * leveltime) & FINEMASK;
 
-  if( !player_bobbing || demo_forcedisablebob)
+  if( !player_bobbing || (demo_forcedisablebob && demoplayback))
     bob = 0;
   else
     bob = FixedMul(player->bob / 2, finesine[angle]);
